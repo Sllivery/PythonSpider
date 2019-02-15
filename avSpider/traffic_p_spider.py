4b3b33bdcc8b7f4653a3ecc3e112a56ef342ee26
LUXness <luxness@LUXnessdeMacBook-Pro.local>
2019-02-15 11:05:31 +0800

    可以使用的单线程版本

--- avSpider/traffic_p_spider.py
+++ avSpider/traffic_p_spider.py
@@ -105,13 +105,15 @@ class Spider:
     def __init__(self, url):
         self.url = url
         self.firefox = webdriver.Firefox()
-        self.firefox.implicitly_wait(10)
+        self.firefox.implicitly_wait(5)
         try:
             # options = webdriver.FirefoxOptions()
             # options.add_argument('-headless')
             # firefox = webdriver.Firefox(options=options)
             # print("浏览器打开")
             self.firefox.get(self.url)
+            self.firefox.find_element_by_xpath('//*[@id="glbm"]/option')
+            self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
             soup = BeautifulSoup(self.firefox.page_source, 'lxml')
             author_list = soup.find('select', id='glbm').contents
             for option_tag in author_list:
@@ -142,7 +144,7 @@ class Spider:
     def check_alert(self):
         try:
             alert = self.firefox.switch_to.alert
-            return True
+            return alert
         except Exception:
             return False
 
@@ -159,8 +161,10 @@ class Spider:
             self.firefox.find_element_by_xpath('//*[@id="nc_1__scale_text"]/span')
             self.firefox.close()
             self.firefox = webdriver.Firefox()
-            self.firefox.implicitly_wait(1000)
+            self.firefox.implicitly_wait(10)
             self.firefox.get(self.url)
+            self.firefox.find_element_by_xpath('//*[@id="glbm"]/option')
+            self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
             return self.send_request(author_num, sort_num)
         except Exception:
             pass
@@ -168,31 +172,85 @@ class Spider:
         captcha_image = self.firefox.find_element_by_xpath(
             "/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[2]/td[2]/div/span/img")
         captcha_image.screenshot("/Users/luxness/Desktop/test.png")
-        # captcha_result = fateadm_api.TestFunc()
+        captcha_result = fateadm_api.TestFunc()
         input_box = self.firefox.find_element_by_xpath("//*[@id='csessionid']")
-        input_box.send_keys('1')
+        input_box.send_keys(captcha_result)
         sure_butt = self.firefox.find_element_by_xpath(
             "/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button[1]")
         sure_butt.click()
-        # 判断alert框是否出现
+        # 判断是否出现Alert
         if self.check_alert():
-            Alert(self.firefox).accept()
+            self.check_alert().accept()
+            time.sleep(0.5)
             return self.send_request(author_num, sort_num)
+        self.firefox.find_element_by_xpath('//*[@id="hdContent"]/table/thead/tr/th[1]')
+        time.sleep(1)
         return self.firefox.page_source
 
 '''
 测试的函数，不用管
 '''
 def get_instance():
-    firefox = webdriver.Firefox()
-    firefox.implicitly_wait(20)
-    firefox.get('http://bj.122.gov.cn/views/vehxhhdpub.html')
-    firefox.quit()
-
+    list1 = {
+        '北京': 'http://bj.122.gov.cn/views/vehxhhdpub.html',
+        '天津': 'http://tj.122.gov.cn/views/vehxhhdpub.html',
+        '河北': 'http://he.122.gov.cn/views/vehxhhdpub.html',
+        '山西': 'http://sx.122.gov.cn/views/vehxhhdpub.html',
+        '内蒙古': 'http://nm.122.gov.cn/views/vehxhhdpub.html',
+        '辽宁': 'http://ln.122.gov.cn/views/vehxhhdpub.html',
+        '吉林': 'http://hl.122.gov.cn/views/vehxhhdpub.html',
+        '黑龙江': 'http://sh.122.gov.cn/views/vehxhhdpub.html',
+        '南京': 'http://nkg.122.gov.cn/views/vehxhhdpub.html',
+        '无锡': 'http://wux.122.gov.cn/views/vehxhhdpub.html',
+        '徐州': 'http://xuz.122.gov.cn/views/vehxhhdpub.html',
+        '常州': 'http://czx.122.gov.cn/views/vehxhhdpub.html',
+        '苏州': 'http://szv.122.gov.cn/views/vehxhhdpub.html',
+        '南通': 'http://ntg.122.gov.cn/views/vehxhhdpub.html',
+        '连云港': 'http://lyg.122.gov.cn/views/vehxhhdpub.html',
+        '淮安': 'http://has.122.gov.cn/views/vehxhhdpub.html',
+        '盐城': 'http://ynz.122.gov.cn/views/vehxhhdpub.html',
+        '扬州': 'http://yzo.122.gov.cn/views/vehxhhdpub.html',
+        '镇江': 'http://zhe.122.gov.cn/views/vehxhhdpub.html',
+        '泰州': 'http://tzs.122.gov.cn/views/vehxhhdpub.html',
+        '宿迁': 'http://suq.122.gov.cn/views/vehxhhdpub.html',
+        '杭州': 'http://hgh.122.gov.cn/views/vehxhhdpub.html',
+        '宁波': 'http://ngb.122.gov.cn/views/vehxhhdpub.html',
+        '温州': 'http://wnz.122.gov.cn/views/vehxhhdpub.html',
+        '嘉兴': 'http://jix.122.gov.cn/views/vehxhhdpub.html',
+        '湖州': 'http://hzh.122.gov.cn/views/vehxhhdpub.html',
+        '绍兴': 'http://sxg.122.gov.cn/views/vehxhhdpub.html',
+        '金华': 'http://jha.122.gov.cn/views/vehxhhdpub.html',
+        '衢州': 'http://quz.122.gov.cn/views/vehxhhdpub.html',
+        '舟山': 'http://zos.122.gov.cn/views/vehxhhdpub.html',
+        '台州': 'http://tzz.122.gov.cn/views/vehxhhdpub.html',
+        '丽水': 'http://lss.122.gov.cn/views/vehxhhdpub.html',
+        '安徽': 'http://ah.122.gov.cn/views/vehxhhdpub.html',
+        '福建': 'http://fj.122.gov.cn/views/vehxhhdpub.html',
+        '江西': 'http://jx.122.gov.cn/views/vehxhhdpub.html',
+        '山东': 'http://sd.122.gov.cn/views/vehxhhdpub.html',
+        '河南': 'http://ha.122.gov.cn/views/vehxhhdpub.html',
+        '湖北': 'http://hb.122.gov.cn/views/vehxhhdpub.html',
+        '湖南': 'http://hn.122.gov.cn/views/vehxhhdpub.html',
+        '广东': 'http://gd.122.gov.cn/views/vehxhhdpub.html',
+        '广西': 'http://gx.122.gov.cn/views/vehxhhdpub.html',
+        '海南': 'http://hi.122.gov.cn/views/vehxhhdpub.html',
+        '重庆': 'http://cq.122.gov.cn/views/vehxhhdpub.html',
+        '四川': 'http://sc.122.gov.cn/views/vehxhhdpub.html',
+        '贵州': 'http://gz.122.gov.cn/views/vehxhhdpub.html',
+        '云南': 'http://yn.122.gov.cn/views/vehxhhdpub.html',
+        '西藏': 'http://xz.122.gov.cn/views/vehxhhdpub.html',
+        '陕西': 'http://sn.122.gov.cn/views/vehxhhdpub.html',
+        '甘肃': 'http://gs.122.gov.cn/views/vehxhhdpub.html',
+        '青海': 'http://qh.122.gov.cn/views/vehxhhdpub.html',
+        '宁夏': 'http://nx.122.gov.cn/views/vehxhhdpub.html',
+        '新疆': 'http://xj.122.gov.cn/views/vehxhhdpub.html'
+    }
+    print(len(list1))
 
 def test():
     pass
 
 
 if __name__ == "__main__":
+    # spider = SpiderSchedule()
     get_instance()
