e1b5f44f4f0248ce615d9793991159b2e9fd98ac
LUXness <luxness@192.168.0.102>
2019-02-15 19:32:30 +0800

    在控制台可以用的版本,现在却少数据导出的功能

--- avSpider/traffic_p_spider.py
+++ avSpider/traffic_p_spider.py
@@ -3,6 +3,7 @@ import fateadm_api
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import *
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -20,19 +21,20 @@ class MakeSomeTips:
     7.因为只需要的是新记录，所以现在不要考虑循环的问题了
 
     现在要解决的事情：
-    把所有硬睡眠改掉
+    跳过url不爬的情况 出现实例：从吉林跳到了黑龙江
     '''
 
 
 class DataOB:
-    def __init__(self, author, sort, num_range, time):
+    def __init__(self, city_name, author, sort, num_range, time):
+        self.city_name = city_name
         self.author = author
         self.sort = sort
         self.num_range = num_range
         self.time = time
 
     def __repr__(self):
-        return "车管所: " + self.author + " 汽车种类: " + self.sort + " 牌照号段: " + self.num_range + " 时间: " + self.time
+        return "省/城市名: " + self.city_name + " |车管所: " + self.author + " |汽车种类: " + self.sort + " |牌照号段: " + self.num_range + " |时间: " + self.time
 
 
 class SpiderSchedule:
@@ -98,37 +100,47 @@ class SpiderSchedule:
             '新疆': 'http://xj.122.gov.cn/views/vehxhhdpub.html'
         }
         for url in self.list:
-            Spider(self.list[url])  # 返回的data应该是形如{'xx车管所':[小汽车,投放号段,投放时间],[大汽车,投放号段,投放时间]}
+            Spider(self.list[url], url)  # 返回的data应该是形如{'xx车管所':[小汽车,投放号段,投放时间],[大汽车,投放号段,投放时间]}
+
+
+class DataExport:
+    def __init__(self, source_data, city_name):
+        self.source_data = source_data
+        self.city_name = city_name
+
+    def demo(self):
+        pass
 
 
 class Spider:
-    def __init__(self, url):
+    def __init__(self, url, city_name):
         self.url = url
+        self.city_name = city_name
+        options = webdriver.FirefoxOptions()
+        options.add_argument('-headless')
         self.firefox = webdriver.Firefox()
-        self.firefox.implicitly_wait(5)
+        self.firefox.implicitly_wait(3)
+        self.firefox.get(self.url)
         try:
-            # options = webdriver.FirefoxOptions()
-            # options.add_argument('-headless')
-            # firefox = webdriver.Firefox(options=options)
-            # print("浏览器打开")
-            self.firefox.get(self.url)
             self.firefox.find_element_by_xpath('//*[@id="glbm"]/option')
             self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
-            soup = BeautifulSoup(self.firefox.page_source, 'lxml')
-            author_list = soup.find('select', id='glbm').contents
-            for option_tag in author_list:
-                author_value = option_tag.get('value')
-                Select(self.firefox.find_element_by_xpath('//*[@id="glbm"]')).select_by_value(author_value)
-                soup2 = BeautifulSoup(self.firefox.page_source, 'lxml')
-                sort_list = soup2.find('select', id='hpzl').contents
-                for option_tag2 in sort_list:
-                    sort_num = option_tag2.get('value')
-                    Select(self.firefox.find_element_by_xpath('//*[@id="hpzl"]')).select_by_value(sort_num)
-                    data_html = self.send_request(author_value, sort_num)
-                    data_ob = self.html_parser(data_html)
-                    print(data_ob)
-        finally:
-            self.firefox.close()
+            time.sleep(0.5)
+        except NoSuchElementException:
+            self.firefox.refresh()
+        soup = BeautifulSoup(self.firefox.page_source, 'lxml')
+        author_list = soup.find('select', id='glbm').contents
+        for option_tag in author_list:
+            author_value = option_tag.get('value')
+            Select(self.firefox.find_element_by_xpath('//*[@id="glbm"]')).select_by_value(author_value)
+            soup2 = BeautifulSoup(self.firefox.page_source, 'lxml')
+            sort_list = soup2.find('select', id='hpzl').contents
+            for option_tag2 in sort_list:
+                sort_num = option_tag2.get('value')
+                Select(self.firefox.find_element_by_xpath('//*[@id="hpzl"]')).select_by_value(sort_num)
+                data_html = self.send_request(author_value, sort_num)
+                data_ob = self.html_parser(data_html)
+                print(data_ob)
+        self.firefox.close()
 
     def html_parser(self, source):
         soup = BeautifulSoup(source, 'lxml')
@@ -137,7 +149,7 @@ class Spider:
             latest_info.remove(' ')
         if latest_info[0].get_text() == '暂无数据':
             return '暂无数据'
-        dataob = DataOB(latest_info[0].get_text(), latest_info[1].get_text().strip(), latest_info[2].get_text(),
+        dataob = DataOB(self.city_name, latest_info[0].get_text(), latest_info[1].get_text().strip(), latest_info[2].get_text(),
                         latest_info[3].get_text())
         return dataob
 
@@ -158,19 +170,25 @@ class Spider:
         self.firefox.find_element_by_xpath("//*[@id='formsearch']/div[2]/div[4]").click()
         # 判断是不是滑动验证
         try:
-            self.firefox.find_element_by_xpath('//*[@id="nc_1__scale_text"]/span')
-            self.firefox.close()
-            self.firefox = webdriver.Firefox()
-            self.firefox.implicitly_wait(10)
-            self.firefox.get(self.url)
-            self.firefox.find_element_by_xpath('//*[@id="glbm"]/option')
-            self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
-            return self.send_request(author_num, sort_num)
-        except Exception:
-            pass
-        # 等待验证码加载出来
-        captcha_image = self.firefox.find_element_by_xpath(
+            captcha_image = self.firefox.find_element_by_xpath(
             "/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[2]/td[2]/div/span/img")
+            time.sleep(0.5)
+        except NoSuchElementException:
+            self.firefox.close()
+            options = webdriver.FirefoxOptions()
+            options.add_argument('-headless')
+            self.firefox = webdriver.Firefox()
+            self.firefox.get(self.url)
+            self.firefox.implicitly_wait(3)
+            try:
+                self.firefox.find_element_by_xpath('//*[@id="glbm"]/option')
+                self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
+                time.sleep(0.5)
+            except NoSuchElementException:
+                self.firefox.refresh()
+            finally:
+                return self.send_request(author_num, sort_num)
+        # 等待验证码加载出来
         captcha_image.screenshot("/Users/luxness/Desktop/test.png")
         captcha_result = fateadm_api.TestFunc()
         input_box = self.firefox.find_element_by_xpath("//*[@id='csessionid']")
@@ -178,12 +196,19 @@ class Spider:
         sure_butt = self.firefox.find_element_by_xpath(
             "/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button[1]")
         sure_butt.click()
+        time.sleep(0.5)
         # 判断是否出现Alert
         if self.check_alert():
             self.check_alert().accept()
+            # 停顿0.5让页面黑幕div消失
             time.sleep(0.5)
             return self.send_request(author_num, sort_num)
-        self.firefox.find_element_by_xpath('//*[@id="hdContent"]/table/thead/tr/th[1]')
+        # 这里try except为了防止程序发生错误自动停止
+        try:
+            self.firefox.find_element_by_xpath('//*[@id="hdContent"]/table/thead/tr/th[1]')
+        except WebDriverException:
+            time.sleep(0.5)
+            return self.send_request(author_num, sort_num)
         time.sleep(1)
         return self.firefox.page_source
 
@@ -191,66 +216,12 @@ class Spider:
 测试的函数，不用管
 '''
 def get_instance():
-    list1 = {
-        '北京': 'http://bj.122.gov.cn/views/vehxhhdpub.html',
-        '天津': 'http://tj.122.gov.cn/views/vehxhhdpub.html',
-        '河北': 'http://he.122.gov.cn/views/vehxhhdpub.html',
-        '山西': 'http://sx.122.gov.cn/views/vehxhhdpub.html',
-        '内蒙古': 'http://nm.122.gov.cn/views/vehxhhdpub.html',
-        '辽宁': 'http://ln.122.gov.cn/views/vehxhhdpub.html',
-        '吉林': 'http://hl.122.gov.cn/views/vehxhhdpub.html',
-        '黑龙江': 'http://sh.122.gov.cn/views/vehxhhdpub.html',
-        '南京': 'http://nkg.122.gov.cn/views/vehxhhdpub.html',
-        '无锡': 'http://wux.122.gov.cn/views/vehxhhdpub.html',
-        '徐州': 'http://xuz.122.gov.cn/views/vehxhhdpub.html',
-        '常州': 'http://czx.122.gov.cn/views/vehxhhdpub.html',
-        '苏州': 'http://szv.122.gov.cn/views/vehxhhdpub.html',
-        '南通': 'http://ntg.122.gov.cn/views/vehxhhdpub.html',
-        '连云港': 'http://lyg.122.gov.cn/views/vehxhhdpub.html',
-        '淮安': 'http://has.122.gov.cn/views/vehxhhdpub.html',
-        '盐城': 'http://ynz.122.gov.cn/views/vehxhhdpub.html',
-        '扬州': 'http://yzo.122.gov.cn/views/vehxhhdpub.html',
-        '镇江': 'http://zhe.122.gov.cn/views/vehxhhdpub.html',
-        '泰州': 'http://tzs.122.gov.cn/views/vehxhhdpub.html',
-        '宿迁': 'http://suq.122.gov.cn/views/vehxhhdpub.html',
-        '杭州': 'http://hgh.122.gov.cn/views/vehxhhdpub.html',
-        '宁波': 'http://ngb.122.gov.cn/views/vehxhhdpub.html',
-        '温州': 'http://wnz.122.gov.cn/views/vehxhhdpub.html',
-        '嘉兴': 'http://jix.122.gov.cn/views/vehxhhdpub.html',
-        '湖州': 'http://hzh.122.gov.cn/views/vehxhhdpub.html',
-        '绍兴': 'http://sxg.122.gov.cn/views/vehxhhdpub.html',
-        '金华': 'http://jha.122.gov.cn/views/vehxhhdpub.html',
-        '衢州': 'http://quz.122.gov.cn/views/vehxhhdpub.html',
-        '舟山': 'http://zos.122.gov.cn/views/vehxhhdpub.html',
-        '台州': 'http://tzz.122.gov.cn/views/vehxhhdpub.html',
-        '丽水': 'http://lss.122.gov.cn/views/vehxhhdpub.html',
-        '安徽': 'http://ah.122.gov.cn/views/vehxhhdpub.html',
-        '福建': 'http://fj.122.gov.cn/views/vehxhhdpub.html',
-        '江西': 'http://jx.122.gov.cn/views/vehxhhdpub.html',
-        '山东': 'http://sd.122.gov.cn/views/vehxhhdpub.html',
-        '河南': 'http://ha.122.gov.cn/views/vehxhhdpub.html',
-        '湖北': 'http://hb.122.gov.cn/views/vehxhhdpub.html',
-        '湖南': 'http://hn.122.gov.cn/views/vehxhhdpub.html',
-        '广东': 'http://gd.122.gov.cn/views/vehxhhdpub.html',
-        '广西': 'http://gx.122.gov.cn/views/vehxhhdpub.html',
-        '海南': 'http://hi.122.gov.cn/views/vehxhhdpub.html',
-        '重庆': 'http://cq.122.gov.cn/views/vehxhhdpub.html',
-        '四川': 'http://sc.122.gov.cn/views/vehxhhdpub.html',
-        '贵州': 'http://gz.122.gov.cn/views/vehxhhdpub.html',
-        '云南': 'http://yn.122.gov.cn/views/vehxhhdpub.html',
-        '西藏': 'http://xz.122.gov.cn/views/vehxhhdpub.html',
-        '陕西': 'http://sn.122.gov.cn/views/vehxhhdpub.html',
-        '甘肃': 'http://gs.122.gov.cn/views/vehxhhdpub.html',
-        '青海': 'http://qh.122.gov.cn/views/vehxhhdpub.html',
-        '宁夏': 'http://nx.122.gov.cn/views/vehxhhdpub.html',
-        '新疆': 'http://xj.122.gov.cn/views/vehxhhdpub.html'
-    }
-    print(len(list1))
-
+    pass
 def test():
     pass
 
 
 if __name__ == "__main__":
-    # spider = SpiderSchedule()
-    get_instance()
+    spider = SpiderSchedule()
+    # get_instance()
+    # spider = Spider('file:///Users/luxness/Desktop/test.html', '徐州市')
