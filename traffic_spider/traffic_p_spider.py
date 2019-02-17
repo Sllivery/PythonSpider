380023a456b7ae84357991d23d65adfec473eeee
LUXness <luxness@192.168.0.102>
2019-02-17 23:43:44 +0800

    完成版，4小时一遍全国的牌号放号情况，无限循环

--- traffic_spider/traffic_p_spider.py
+++ traffic_spider/traffic_p_spider.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import *
 from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -21,13 +22,15 @@ class SpiderSchedule:
     根据队列中的url来循环或者选取url来给爬虫用
     """
     def __init__(self):
+        options = webdriver.FirefoxOptions()
+        options.add_argument('-headless')
         self.firefox = webdriver.Firefox()
-        self.firefox.implicitly_wait(3)
+        self.firefox.implicitly_wait(10)
         self.list = [
-            # 'http://bj.122.gov.cn/views/vehxhhdpub.html',
-            # 'http://tj.122.gov.cn/views/vehxhhdpub.html',
-            # 'http://he.122.gov.cn/views/vehxhhdpub.html',
-            'http://sx.122.gov.cn/views/vehxhhdpub.html',
+            'http://bj.122.gov.cn/views/vehxhhdpub.html',
+            'http://tj.122.gov.cn/views/vehxhhdpub.html',
+            'http://he.122.gov.cn/views/vehxhhdpub.html',
+            'https://sx.122.gov.cn/views/vehxhhdpub.html',
             'http://nm.122.gov.cn/views/vehxhhdpub.html',
             'http://ln.122.gov.cn/views/vehxhhdpub.html',
             'http://hl.122.gov.cn/views/vehxhhdpub.html',
@@ -83,7 +86,7 @@ class SpiderSchedule:
         if termination_info.url == "":
             return self.list
         if termination_info.url != "":
-            new_list = self.list[self.list.index(termination_info.url):len(self.list)]
+            new_list = self.list[self.list.index(termination_info.url):]
             return new_list
 
     def run_spider(self):
@@ -94,7 +97,6 @@ class SpiderSchedule:
                         Spider(url, termination_info.author_num, termination_info.sort_num, self.firefox)
             except Exception as e:
                 termination_time += 1
-                print(e)
             else:
                 print("列表抓取完毕，等待60秒重新开始")
                 termination_info.url = ""
@@ -114,50 +116,58 @@ class Spider:
         self.firefox = firefox
         self.firefox.get(self.url)
         termination_info.url = self.url
-        try:
-            self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
-        except NoSuchElementException:
-            self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option')
+        self.firefox.find_element_by_xpath('//*[@id="hpzl"]/option[1]')
         soup = BeautifulSoup(self.firefox.page_source, 'lxml')
         author_tag_list = soup.find('select', id='glbm').contents
         author_list = []
         for author_tag in author_tag_list:
-            author_list.append(author_tag.get('value'))
+            author_list.append(author_tag.get_text())
         if termination_info.author_num != "":
             try:
-                author_list = author_list[author_list.index(termination_info.author_num):len(author_list)]
+                author_list = author_list[author_list.index(termination_info.author_num):]
             except ValueError as e:
                 author_list = author_list
-        # 这里加个bs4 find城市名,给self.city_name
         self.city_name = soup.find('span', class_='header-logo-top fL').get_text()[0:2]
         for option in author_list:
             termination_info.author_num = option
-            author_value = option
-            Select(self.firefox.find_element_by_xpath('//*[@id="glbm"]')).select_by_value(author_value)
+            Select(self.firefox.find_element_by_xpath('//*[@id="glbm"]')).select_by_visible_text(option)
             soup2 = BeautifulSoup(self.firefox.page_source, 'lxml')
             sort_list = soup2.find('select', id='hpzl').contents
-            for option_tag2 in sort_list:
-                sort_num = option_tag2.get('value')
-                termination_info.sort_num = sort_num
-                Select(self.firefox.find_element_by_xpath('//*[@id="hpzl"]')).select_by_value(sort_num)
-                data_html = self.send_request(author_value, sort_num)
+            sort_content = []
+            for sort_tag in sort_list:
+                sort_content.append(sort_tag.get_text())
+            if termination_info.sort_num != "":
+                try:
+                    sort_content = sort_content[sort_content.index(termination_info.sort_num):]
+                except ValueError as e:
+                    sort_content = sort_content
+            for option_tag2 in sort_content:
+                termination_info.sort_num = option_tag2
+                Select(self.firefox.find_element_by_xpath('//*[@id="hpzl"]')).select_by_visible_text(option_tag2)
+                data_html = self.send_request(option, option_tag2)
                 data_ob = self.html_parser(data_html)
+                termination_info.sort_num = ""
 
-    def send_request(self,author_num, sort_num):
+    def send_request(self, author_num, sort_num):
         # 保存当前循环的状态信息
-        self.firefox.delete_all_cookies()
+        if self.url == 'https://sx.122.gov.cn/views/vehxhhdpub.html':
+            self.firefox.refresh()
         author_num = author_num
         sort_num = sort_num
-        Select(self.firefox.find_element_by_xpath('//*[@id="glbm"]')).select_by_value(author_num)
-        Select(self.firefox.find_element_by_xpath('//*[@id="hpzl"]')).select_by_value(sort_num)
+        Select(self.firefox.find_element_by_xpath('//*[@id="glbm"]')).select_by_visible_text(author_num)
+        Select(self.firefox.find_element_by_xpath('//*[@id="hpzl"]')).select_by_visible_text(sort_num)
         # 找到查询的按钮点一下
-        self.firefox.find_element_by_xpath("//*[@id='formsearch']/div[2]/div[4]").click()
-        captcha_image = self.firefox.find_element_by_xpath(
-            "/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[2]/td[2]/div/span/img")
-        time.sleep(0.5)
-        captcha_image.screenshot("/Users/luxness/Desktop/test.png")
-        # while test.png == failed.png:
-        #     # 点击刷新，截图，记录次数，>5次
+        search_butt = self.firefox.find_element_by_xpath('//*[@id="formsearch"]/div[2]/div[4]')
+        search_butt.click()
+        try:
+            captcha_image = self.firefox.find_element_by_xpath("/html/body/div[1]/div/table/tbod"
+                                                               "y/tr[2]/td[2]/div/table/tbody/tr[2]/td[2]/div/span/img")
+            time.sleep(1)
+            captcha_image.screenshot("/Users/luxness/Desktop/test.png")
+        except NoSuchElementException as e:
+            self.firefox.delete_all_cookies()
+            self.firefox.refresh()
+            return self.send_request(author_num, sort_num)
         captcha_result = fateadm_api.TestFunc()
         input_box = self.firefox.find_element_by_xpath("//*[@id='csessionid']")
         input_box.send_keys(captcha_result)
@@ -165,13 +175,13 @@ class Spider:
         sure_butt = self.firefox.find_element_by_xpath(
             "/html/body/div[1]/div/table/tbody/tr[2]/td[2]/div/table/tbody/tr[3]/td/div/button[1]")
         sure_butt.click()
+        time.sleep(0.6)
         # 判断是否出现Alert
         if self.check_alert():
             self.check_alert().accept()
             # 停顿0.5让页面黑幕div消失
-            time.sleep(0.5)
+            time.sleep(5)
             return self.send_request(author_num, sort_num)
-        time.sleep(1)
         self.firefox.delete_all_cookies()
         return self.firefox.page_source
 
@@ -203,8 +213,10 @@ def get_instance():
 
 
 def test():
-    list1 = ["hey", "you"]
-    print(list1.index("you"))
+    list1 = ["hey", "you", "what"]
+    print(list1.index("what"))
+    print(len(list1))
+    print(list1[1:])
 
 
 if __name__ == "__main__":
